845a9930aab57d0c025e18c09e4f090c20fc95bc
himalf <himal.fullel15@gmail.com>
2025-01-08 11:08:40 +0545

    feat: add a users dto for validation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,17 @@
-export class CreateUserDto {}
+import { IsEmail, IsEnum, IsString } from 'class-validator';
+import { UserRole } from './userRole.enum';
+
+export class CreateUserDto {
+  @IsString()
+  name: string;
+  @IsString()
+  address: string;
+  @IsEmail()
+  email: string;
+  @IsString()
+  password;
+  @IsEnum(UserRole)
+  role: UserRole;
+  @IsString()
+  profile_image: string;
+}
