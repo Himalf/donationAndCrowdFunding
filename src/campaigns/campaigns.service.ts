d31242578575c81f11193a11b7873e6a39a4418d
himalf <himal.fullel15@gmail.com>
2025-01-08 15:30:18 +0545

    feat: create a new campaign in create method

--- src/campaigns/campaigns.service.ts
+++ src/campaigns/campaigns.service.ts
@@ -1,11 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateCampaignDto } from './dto/create-campaign.dto';
 import { UpdateCampaignDto } from './dto/update-campaign.dto';
+import { Repository } from 'typeorm';
+import { Campaign } from './entities/campaign.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/users/entities/user.entity';
 
 @Injectable()
 export class CampaignsService {
-  create(createCampaignDto: CreateCampaignDto) {
-    return 'This action adds a new campaign';
+  constructor(
+    @InjectRepository(Campaign)
+    private readonly campaignRepository: Repository<Campaign>,
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+  async create(createCampaignDto: CreateCampaignDto) {
+    const users = await this.userRepository.findOne({
+      where: { user_id: createCampaignDto.user_id },
+    });
+    if (!users) {
+      throw new NotFoundException(
+        `The user with id:${users.user_id} is not found`,
+      );
+    }
+    const campaignData = this.campaignRepository.create({
+      user: users,
+      ...createCampaignDto,
+    });
+    return await this.campaignRepository.save(campaignData);
   }
 
   findAll() {
