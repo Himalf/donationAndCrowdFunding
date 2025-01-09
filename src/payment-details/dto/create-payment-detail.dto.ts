591fd513f95603b3fbdcce6b03b92e0f13a9a78b
himalf <himal.fullel15@gmail.com>
2025-01-09 15:09:50 +0545

    feat: add dto for validation with enum

---
+++ src/payment-details/dto/create-payment-detail.dto.ts
@@ -0,0 +1,14 @@
+import { IsEnum, IsNumber, IsString } from 'class-validator';
+import { PaymentMethod } from './paymentMethod.enum';
+import { PaymentStatus } from './paymentstatus.enum';
+
+export class CreatePaymentDetailDto {
+  @IsNumber()
+  donation_id: number;
+  @IsEnum(PaymentMethod)
+  payment_method: PaymentMethod;
+  @IsNumber()
+  transaction_id: number;
+  @IsEnum(PaymentStatus)
+  status: PaymentStatus;
+}
---
+++ src/payment-details/dto/paymentMethod.enum.ts
@@ -0,0 +1,9 @@
+export enum PaymentMethod {
+  CREDIT_CARD = 'Credit Card',
+  DEBIT_CARD = 'Debit Card',
+  ESEWA = 'esewa',
+  KHALTI = 'khalti',
+  BANK_TRANSFER = 'Bank Transfer',
+  CRYPTOCURRENCY = 'Cryptocurrency',
+  CASH = 'Cash',
+}
