5d1d2c5ef3484ba23a2364b4d14a7d1964a73d9c
himalf <himal.fullel15@gmail.com>
2025-01-09 12:29:39 +0545

    feat: add donation entity

--- src/donations/entities/donation.entity.ts
+++ src/donations/entities/donation.entity.ts
@@ -1,1 +1,20 @@
-export class Donation {}
+import {
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Donation {
+  @PrimaryGeneratedColumn()
+  donation_id: number;
+  @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  donated_at: Date;
+  @UpdateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+  })
+  updated_at: Date;
+}
