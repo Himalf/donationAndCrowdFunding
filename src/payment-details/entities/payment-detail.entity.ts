25be78065fc9bd38d0461c1227ef1be1538a20aa
himalf <himal.fullel15@gmail.com>
2025-01-09 16:21:56 +0545

    feat: add paymentDetail entity

--- src/payment-details/entities/payment-detail.entity.ts
+++ src/payment-details/entities/payment-detail.entity.ts
@@ -1,1 +1,27 @@
-export class PaymentDetail {}
+import { Donation } from 'src/donations/entities/donation.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { PaymentMethod } from '../dto/paymentMethod.enum';
+import { PaymentStatus } from '../dto/paymentstatus.enum';
+
+@Entity()
+export class PaymentDetail {
+  @PrimaryGeneratedColumn()
+  paymentDetail_id: number;
+  @OneToOne(() => Donation, (donation) => donation.paymentDetail)
+  donation: Donation;
+  @Column({ type: 'enum', enum: PaymentMethod })
+  payment_method: PaymentMethod;
+  @Column({ unique: true })
+  transaction_id: number;
+  @Column({ type: 'enum', enum: PaymentStatus })
+  status: PaymentStatus;
+  @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  processed_at: Date;
+}
