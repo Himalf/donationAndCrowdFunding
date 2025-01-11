3d4b385c4cda4461d06e0ba7c9def98ff9793335
himalf <himal.fullel15@gmail.com>
2025-01-11 13:10:03 +0545

    feat: add swagger implementation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('The Donation and CrowdFunding system API')
+    .setDescription(
+      'The donation and crowdfunding system that allows us to donate as a donot in various campaigns',
+    )
+    .setVersion('1.0')
+    .addTag('Donations')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('', app, documentFactory);
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
