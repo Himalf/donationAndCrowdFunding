ab5b338dd84ddee62e96652a2853cf80b0feb9d2
himalf <himal.fullel15@gmail.com>
2025-01-13 10:10:19 +0545

    feat: add api property in campaign dto

--- src/campaigns/dto/create-campaign.dto.ts
+++ src/campaigns/dto/create-campaign.dto.ts
@@ -1,19 +1,27 @@
 import { IsEnum, IsNumber, IsString } from 'class-validator';
 import { CampaignStatus } from './campaignStatus.enum';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateCampaignDto {
+  @ApiProperty()
   @IsString()
   title: string;
+  @ApiProperty()
   @IsString()
   description: string;
+  @ApiProperty()
   @IsNumber()
   goal_amount: number;
+  @ApiProperty()
   @IsString()
   start_date: string;
+  @ApiProperty()
   @IsString()
   end_date: string;
+  @ApiProperty()
   @IsEnum(CampaignStatus)
   status: CampaignStatus;
+  @ApiProperty()
   @IsNumber()
   user_id: number;
 }
