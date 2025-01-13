9cd469bf0f2487a300421bf35c1c84c277a54723
himalf <himal.fullel15@gmail.com>
2025-01-13 10:15:26 +0545

    fix: add type decorators in dto

--- src/donations/dto/create-donation.dto.ts
+++ src/donations/dto/create-donation.dto.ts
@@ -1,5 +1,10 @@
+import { IsNumber } from 'class-validator';
+
 export class CreateDonationDto {
+  @IsNumber()
   user_id: number;
+  @IsNumber()
   amount: number;
+  @IsNumber()
   campaign_id: number;
 }
