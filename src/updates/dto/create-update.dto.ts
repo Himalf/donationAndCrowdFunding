b7804a0a99b40d596b5f188d319bc27f2a98435d
himalf <himal.fullel15@gmail.com>
2025-01-13 10:16:13 +0545

    chore: add @ApiProperty() decorators in updates dto

--- src/updates/dto/create-update.dto.ts
+++ src/updates/dto/create-update.dto.ts
@@ -1,8 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber, IsString } from 'class-validator';
 
 export class CreateUpdateDto {
+  @ApiProperty()
   @IsString()
   message: string;
+  @ApiProperty()
   @IsNumber()
   campaign_id: number;
 }
