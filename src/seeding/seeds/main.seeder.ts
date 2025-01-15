e6a970507841452d7ee52ea71bdc345034efc8b8
himalf <himal.fullel15@gmail.com>
2025-01-15 16:54:22 +0545

    feat: seed the campaigns with users

--- src/seeding/seeds/main.seeder.ts
+++ src/seeding/seeds/main.seeder.ts
@@ -1,3 +1,5 @@
+import { faker } from '@faker-js/faker/.';
+import { Campaign } from 'src/campaigns/entities/campaign.entity';
 import { User } from 'src/users/entities/user.entity';
 import { DataSource } from 'typeorm';
 import { Seeder, SeederFactoryManager } from 'typeorm-extension';
@@ -7,7 +9,20 @@ export default class MainSeeder implements Seeder {
     dataSource: DataSource,
     factoryManager: SeederFactoryManager,
   ): Promise<any> {
+    const campaignRepository = dataSource.getRepository(Campaign);
     const userFactory = factoryManager.get(User);
+    const campaignFactory = factoryManager.get(Campaign);
     const users = await userFactory.saveMany(7);
+    const campaign = await Promise.all(
+      Array(17)
+        .fill('')
+        .map(async () => {
+          const made = await campaignFactory.make({
+            user: faker.helpers.arrayElement(users),
+          });
+          return made;
+        }),
+    );
+    await campaignRepository.save(campaign);
   }
 }
